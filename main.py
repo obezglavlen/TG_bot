6837f06aa7ffc0be6385504f1b1f9d5ca277b8ad
obezglavlen <pcgalaxy123@gmail.com>
2022-05-01 22:24:54 +0300

    heroku

--- main.py
+++ main.py
@@ -12,21 +12,23 @@ server = Flask(__name__)
 
 @server.route("/" + TOKEN, methods=["POST"])
 def getMessage():
-    BOT.process_new_updates([Update.de_json(request.stream.read().decode("utf-8"))])
+    BOT.process_new_updates(
+        [Update.de_json(request.stream.read().decode("utf-8"))])
     return "!", 200
 
 
 @server.route("/")
 def webhook():
     BOT.remove_webhook()
-    BOT.set_webhook(url=HEROKU + TOKEN, allowed_updates=["message", "callback_query"])
+    BOT.set_webhook(url=HEROKU + TOKEN,
+                    allowed_updates=["message", "callback_query"])
     return "!", 200
 
 
 if __name__ == "__main__":
     # If you host bot on Heroku, you can use Flask server for webhook
-    # server.run(host="0.0.0.0", port=PORT)
+    server.run(host="0.0.0.0", port=PORT)
 
     # If you host bot on localhost, you can use bot polling
-    BOT.remove_webhook()
-    BOT.infinity_polling(allowed_updates=["message", "callback_query"])
+    # BOT.remove_webhook()
+    # BOT.infinity_polling(allowed_updates=["message", "callback_query"])
