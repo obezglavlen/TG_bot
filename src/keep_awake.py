9df0e21bcb794e65ae9903cfeff01f893973fe20
obezglavlen <pcgalaxy123@gmail.com>
2022-05-20 18:18:36 +0300

    awake 30 min

--- src/keep_awake.py
+++ src/keep_awake.py
@@ -2,6 +2,7 @@ import requests
 import threading
 from src.config import HEROKU, TOKEN
 
+
 def setInterval(func, sec):
     def func_wrapper():
         setInterval(func, sec)
@@ -10,4 +11,5 @@ def setInterval(func, sec):
     t.start()
     return t
 
-setInterval(lambda: requests.get(HEROKU + TOKEN), 3540)
+
+setInterval(lambda: requests.get(HEROKU + TOKEN), 1740)
