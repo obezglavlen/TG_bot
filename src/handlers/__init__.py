8516ead68fec5ebead809fc7d0e78db2265a8c42
obezglavlen <pcgalaxy123@gmail.com>
2022-08-30 17:27:50 +0300

    remove anime commands

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,8 +1,8 @@
-from . import anime
+# from . import anime
 from . import voice
 from . import command
 from . import contact
 from . import dick
 from . import sticker
 
-__all__ = ["anime", "command", "contact", "dick", "sticker", "voice"]
+__all__ = ["command", "contact", "dick", "sticker", "voice"]
